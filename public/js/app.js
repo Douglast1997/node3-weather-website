fb97fa7ea6e35aaf6c265588488e74c86d6c8821
douglast1997 <dtrps@cin.ufpe.br>
2020-08-24 12:14:13 -0300

    Clear old code

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
+/*fetch('http://puzzle.mead.io/puzzle').then((response) => {
   response.json().then((data) => {
     console.log(data)
   })
