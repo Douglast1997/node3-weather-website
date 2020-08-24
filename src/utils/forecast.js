9085f317b030e5ab0deae33f64ead19fb442c760
douglast1997 <dtrps@cin.ufpe.br>
2020-08-24 10:46:52 -0300

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) =>{
+ 
+  const url = 'http://api.weatherstack.com/current?access_key=d6e638343914aeecf515261f2596e13b&query='+ latitude +','+ longitude +'&units=m'
+
+  request({url, json: true}, (error, {body}) => {
+    if(error){
+      callback('Unable to connect to weather services!',undefined)
+    } else if(body.error){
+      callback('Unable to find Location',undefined)
+    } else {
+      callback(undefined, ' It is currently '+ body.current.temperature + ' degrees out. There is a ' + body.current.precip + '% chance of rain'
+      )
+    }
+  })
+}
+
+
+module.exports = forecast
